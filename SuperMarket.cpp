f7a2b38397f62ef48fedc5a80d3afe857e762704
cresb-49 <carlosbpac@gmial.com>
2021-02-22 18:29:08 -0600

    Inicializacion de la pila de carretas en windows

--- SuperMarket.cpp
+++ SuperMarket.cpp
@@ -14,6 +14,7 @@ class carreta{
         carreta();
         carreta* getSiguiente();
         void setCarreta(carreta*);
+        int getId();
 };
 
 carreta::carreta(int _id){
@@ -28,6 +29,10 @@ carreta* carreta::getSiguiente(){
 
 void carreta::setCarreta(carreta* _siguinte){
     siguinte=_siguinte;
+}
+
+int carreta::getId(){
+    return id;
 }
 
 //----------------------------CLASE CLIENTE-------------------------------------------------------
@@ -108,11 +113,13 @@ int numCarretas=0,numCajas=0;
 string reader;
 
 carreta *pilaCarreta1,*pilaCarreta2;
-
+cliente *colaEntrada,*colaCobro;
 
 //////DEFINICION DE METODOS DEL PROGRAMA
+void llenarPilaCarretas();
 void agregarCarreta(carreta *nuevaCarreta,int pila);
 carreta* tomarCarreta(int pila);
+void imprimirCarretas(int pila);
 //////FIN DE DEFINICION DE METODOS DEL PROGRAMA
 
 
@@ -120,7 +127,7 @@ carreta* tomarCarreta(int pila);
 int main(){
 
     printf("Bienvenido a la simulacion de SuperMercado\n");
-
+    //////////ASIGNACION DE DATOS DEL FUNCIONAMIENTO DEL PROGRAMA
     while (numCarretas == 0)
     {
         printf("Ingrese el numero de Carretas: ");
@@ -149,17 +156,53 @@ int main(){
         }
         
     }
-    
-    
+    ///////FIN DE PARAMETROS DE ASIGNACION
     printf("El numero de carretas son: %d, el numero de cajas son: %d \n",numCarretas,numCajas);
+    llenarPilaCarretas();
+    imprimirCarretas(1);
+    imprimirCarretas(2);
+
+
+
     return 0;
+}
+
+void llenarPilaCarretas(){
+    int mitad1=0,mitad2=0;
+
+    mitad1 = numCarretas/2;
+    mitad2 = numCarretas-mitad1;
+
+    for (int i = 1; i <= mitad1; i++)
+    {
+        agregarCarreta(new carreta(i),1);
+    }
+
+    for (int i = (mitad1+1); i <= numCarretas; i++)
+    {
+        agregarCarreta(new carreta(i),2);
+    }
 }
 
 void agregarCarreta(carreta *nuevaCarreta,int pila){
     if(pila==1){
-
+        if(pilaCarreta1==NULL){
+            pilaCarreta1 = nuevaCarreta;
+        }else{
+            carreta *tmp;
+            tmp = pilaCarreta1;
+            nuevaCarreta->setCarreta(tmp);
+            pilaCarreta1 = nuevaCarreta;
+        }
     }else if(pila ==2){
-
+        if(pilaCarreta2==NULL){
+            pilaCarreta2 = nuevaCarreta;
+        }else{
+            carreta *tmp;
+            tmp = pilaCarreta2;
+            nuevaCarreta->setCarreta(tmp);
+            pilaCarreta2 = nuevaCarreta;
+        }
     }else{
         printf("Error al agregar la carreta, ruta por defecto pila 1\n");
         agregarCarreta(nuevaCarreta,1);
@@ -178,3 +221,40 @@ carreta* tomarCarreta(int pila){
     }
     return NULL;
 }
+
+void imprimirCarretas(int pila){
+    if(pila == 1){
+        printf("IMPRECION DE PILA NUMERO 1\n");
+        if(pilaCarreta1==NULL){
+            printf("Pila numero 1 vacia\n");
+        }else{
+            carreta *tmp;
+            tmp = pilaCarreta1;
+            while (tmp != NULL)
+            {
+                printf("Carreta id: %d, pila ubicacion: 1\n",tmp->getId());
+                tmp = tmp->getSiguiente();
+            }
+            
+        }
+
+    }else if(pila == 2){
+        printf("IMPRECION DE PILA NUMERO 2\n");
+        if(pilaCarreta2==NULL){
+            printf("Pila numero 2 vacia\n");
+        }else{
+            carreta *tmp;
+            tmp = pilaCarreta2;
+            while (tmp != NULL)
+            {
+                printf("Carreta id: %d, pila ubicacion: 1\n",tmp->getId());
+                tmp = tmp->getSiguiente();
+            }
+            
+        }
+    }else{
+        printf("Error de imprecion en pila de carretas\n");
+        printf("Imprecion por defecto:\n");
+    }
+}
+
